0cc2fb6875800c5afc274aa689b2146803fa7b05
Claudio Haffner <chaffner@fdm-spa.com>
2018-07-30 07:57:54 +0000

    added Hello program

---
+++ Hello.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, package World");
+    }
+
+}
